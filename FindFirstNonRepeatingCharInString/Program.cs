75ce3af19124538f055ef58be60a2ae21829e28f
Admin <tech.ajitsakhare@gmail.com>
2026-06-18 21:25:24 +0530

    Add project files.

---
+++ FindFirstNonRepeatingCharInString/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    static void Main()
+    {
+
+        string inputString = "aabbcdde";
+
+        Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+
+        foreach(char ch in inputString)
+        {
+            if (keyValuePairs.ContainsKey(ch))
+            {
+                keyValuePairs[ch]++;
+            }
+            else
+            {
+                keyValuePairs[ch] = 1;
+            }
+        }
+        foreach(char ch in inputString) {
+            if (keyValuePairs[ch] == 1)
+            {
+                Console.WriteLine("first non repeating character is = " + ch);
+                break;
+            }
+        }
+    }
+}
